265c0463a6c42b5a333b5a9fa10eb4ec5dead92b
sakzgupzzz <83149825+sakzgupzzz@users.noreply.github.com>
2024-07-13 14:26:18 -0400

    changed dev env to angular and rect

---
+++ my-website/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const pool = require('./db');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/data', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM mytable');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
